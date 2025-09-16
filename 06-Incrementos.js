2a20504d4fc2febc91ae6f870d30d9cec1d4ee88
Kevin <loaizakevin75@gmail.com>
2025-09-16 16:13:33 -0500

    Se hicieron nuevos ejercicios

---
+++ 06-Incrementos.js
@@ -0,0 +1,21 @@
+//Laura Alexandra Loaiza Atehortua
+//concatenacion y template string
+
+//score = 10;
+// Incremento de una unidad
+//score++;
+//console.log(score++);
+//console.log(score);
+
+//console.log(++score);
+//console.log(--score);
+
+
+let score = 20;
+score -= 5;
+
+console.log(score)
+let numer1 = 15.5;
+let result = Math.ceil(numer1);
+
+console.log(result);
---
+++ 07-concatenacion-temporal.js
@@ -0,0 +1,14 @@
+// Laura Alexandra Loaiza Atehortua
+// Incrementos en JS
+
+const userName = 'Alexa1y3';
+const userEmail = 'loaizalaura411@gmail.com'
+
+console.log(userName + " " + userEmail);
+// En algunas ocasiones podemos concatenar con comas(,)
+console.log("Nombre de usuario:" , userName , "Correo de usuario:" , userEmail);
+
+//Nueva sintaxis
+// Template string - String Literals 
+
+console.log(`Nombre de usuario: ${userName} Email: ${userEmail}`);
